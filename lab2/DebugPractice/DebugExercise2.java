50b6a4924e7c1cb0e3f14a446fd3c30fb3c38812
Jack Chiu <skypejackchiu@gmail.com>
2020-10-26 22:23:23 +0900

    completed lab2 catenate part

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Exercise to showcase the step over button.
  * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
@@ -7,12 +9,14 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+        int max;
+        if(a >= b) {
+            max = a;
+        }
+        else{
+            max = b;
+        }
 
-        int max = b & w | a & z;
         return max;
     }
 
@@ -21,20 +25,7 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */    
     public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a + b;
     }
 
     /** Returns a new array where entry i is the max of
@@ -60,7 +51,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
