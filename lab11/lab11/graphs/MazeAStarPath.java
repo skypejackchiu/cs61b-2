9e11d1a08f5027c9cb40d6e975b7823deb81acbf
Jack Chiu <skypejackchiu@gmail.com>
2021-02-26 09:32:11 +0900

    added MazeAStarPath

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Comparator;
+
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  *  @author Josh Hug
  */
@@ -18,9 +23,35 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
+    private class VNode  {
+        private int v;
+        private int h;
+        private Integer priority;
+
+        private VNode(int v, int dist) {
+            this.v = v;
+            priority = dist + h(v);
+        }
+    }
+
+    private class VNodeComparator implements Comparator<VNode> {
+        @Override
+        public int compare(VNode node1, VNode node2) {
+            return node1.priority.compareTo(node2.priority);
+        }
+    }
+
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int estimatedDistance;
+
+        int vX = maze.toX(v);
+        int vY = maze.toY(v);
+        int tX = maze.toX(t);
+        int tY = maze.toY(t);
+        estimatedDistance = Math.abs(vX- tX) + Math.abs(vY - tY);
+
+        return estimatedDistance;
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -32,6 +63,29 @@ public class MazeAStarPath extends MazeExplorer {
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
         // TODO
+        MinPQ<VNode> fringe = new MinPQ<>(maze.V(), new VNodeComparator());
+        VNode currentNode = new VNode(s, distTo[s]);
+        fringe.insert(currentNode);
+
+        while (!fringe.isEmpty()) {
+            currentNode = fringe.delMin();
+            marked[currentNode.v] = true;
+            announce();
+            if (currentNode.v == t) {
+                return;
+            }
+            for (int w : maze.adj(currentNode.v)) {
+                if (distTo[currentNode.v] + 1 < distTo[w]) {
+                    distTo[w] = distTo[currentNode.v] + 1;
+                    edgeTo[w] = currentNode.v;
+                }
+
+                if (!marked[w]) {
+                    VNode WNode = new VNode(w, distTo[w]);
+                    fringe.insert(WNode);
+                }
+            }
+        }
     }
 
     @Override
